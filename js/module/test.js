8e6f14b18de71af6943381cfc78bcf8159cdf3c0
yaoyao1987 <yaolingyan1987@126.com>
2016-03-10 23:29:44 +0800

    js
    test

---
+++ js/module/test.js
@@ -0,0 +1,33 @@
+﻿/*
+ *公告列表
+ */
+define(['jquery', 'underscore', 'domReady', 'common',
+    'text!' + window.basePath + 'html/index/tpl/test.tpl',
+    'css!' + window.basePath + 'css/module/test.css'
+], function($, _, domReady, common, tpl) {
+
+    function Test() {
+        this._init();
+        this._initEvent();
+    }
+
+    Test.prototype = {
+        _init: function() {
+            appView.html(tpl);
+
+            domReady(function() {
+                loadingView.hide();
+            });
+        },
+        _initEvent: function() {
+            $('.ui-input').on('focus', function() {
+                $('.ui-textarea-container').removeClass('hide');
+                $('.ui-textarea-container textarea').on('focus');
+            });
+        }
+    };
+
+    return function() {
+        new Test();
+    };
+});
