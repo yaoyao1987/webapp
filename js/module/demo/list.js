56010b7a1fd8066eef2228b03da9db91f7ee184f
yaoyao1987 <yaolingyan1987@126.com>
2016-03-19 09:57:33 +0800

    demo
    demo的js

---
+++ js/module/demo/list.js
@@ -0,0 +1,31 @@
+﻿/*
+ *列表
+ */
+define(['jquery', 'underscore', 'domReady', 'common', 'IScrollLoadData',
+    'text!' + window.basePath + 'html/tpl/demo/list.tpl'
+], function($, _, domReady, common, IScrollLoadData, tpl) {
+
+    function Test() {
+        this._init();
+        this._initEvent();
+    }
+
+    Test.prototype = {
+        _init: function() {
+            var self = this;
+
+            appView.html(tpl);
+
+            domReady(function() {
+                loadingView.hide();
+            });
+        },
+        _initEvent: function() {
+
+        }
+    };
+
+    return function() {
+        new Test();
+    };
+});
